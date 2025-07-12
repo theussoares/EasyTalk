5f08c2b03d8922f13d8dc8978b2940ef71395bc3
Matheus Ferreira Soares <matheusfs774@gmail.com>
2025-07-12 12:13:15 -0400

    feat: trocando a disposição das palavras

--- components/lessons/VocabSection.vue
+++ components/lessons/VocabSection.vue
@@ -3,7 +3,7 @@
     <!-- Formato antigo (compatibilidade) - uma única seção sem título -->
     <div v-if="vocab && vocab.length > 0" class="p-4 bg-sky-50 border-l-4 border-sky-500 rounded-r-lg dark:bg-sky-900/50 dark:border-sky-500">
       <h3 class="text-sm font-semibold text-sky-800 dark:text-sky-300 mb-3 uppercase tracking-wider">Vocabulary</h3>
-      <div class="flex flex-wrap gap-x-6 gap-y-2">
+      <div class="flex flex-wrap justify-evenly gap-y-2">
         <span v-for="(word, index) in vocab" :key="index" class="font-mono text-lg text-gray-700 dark:text-gray-300">
           {{ word }}
         </span>
--- components/lessons/VocabSectionItem.vue
+++ components/lessons/VocabSectionItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mb-6 p-4 mt-2 bg-sky-50 border-l-4 border-sky-500 rounded-r-lg dark:bg-sky-900/50 dark:border-sky-500">
-    <div class="flex flex-wrap gap-x-6 gap-y-2">
+    <div class="flex flex-wrap justify-evenly gap-y-2">
       <span v-for="(word, index) in words" :key="index" class="font-mono text-lg text-gray-700 dark:text-gray-300">
         {{ word }}
       </span>
