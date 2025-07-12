93a188b1f335646416f96b3737920c2ac982ef30
Matheus Ferreira Soares <matheusfs774@gmail.com>
2025-07-12 12:09:13 -0400

    fix: mudando coisas simples

--- components/lessons/VocabSectionItem.vue
+++ components/lessons/VocabSectionItem.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="mb-6 p-4 bg-sky-50 border-l-4 border-sky-500 rounded-r-lg dark:bg-sky-900/50 dark:border-sky-500">
-    <h3 class="text-sm font-semibold text-sky-800 dark:text-sky-300 mb-3 uppercase tracking-wider">
-      {{ title || 'Vocabulary' }}
-    </h3>
+  <div class="mb-6 p-4 mt-2 bg-sky-50 border-l-4 border-sky-500 rounded-r-lg dark:bg-sky-900/50 dark:border-sky-500">
     <div class="flex flex-wrap gap-x-6 gap-y-2">
       <span v-for="(word, index) in words" :key="index" class="font-mono text-lg text-gray-700 dark:text-gray-300">
         {{ word }}
