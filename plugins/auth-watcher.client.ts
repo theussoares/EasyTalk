00e4a13e1d0e54d9e2cd6be9df79b4a82fab0a76
Matheus Ferreira Soares <matheusfs774@gmail.com>
2025-07-14 16:16:41 -0400

    feat: removendo arquivos não utilizados

--- plugins/auth-watcher.client.ts
+++
@@ -1,5 +0,0 @@
-// plugins/auth-watcher.client.ts
-// Alternativa mais simples que usa onMounted na página
-export default defineNuxtPlugin(() => {
-  // Plugin vazio - a lógica de auth será movida para as páginas que precisam
-});
